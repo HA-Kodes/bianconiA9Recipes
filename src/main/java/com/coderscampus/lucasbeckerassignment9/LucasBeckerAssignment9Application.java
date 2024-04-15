091382b12c2cabd5ed576322da6b792007a2d6c1
Lucas Becker <lucasbecker.dev@gmail.com>
2024-04-15 16:24:32 -0400

    initial commit

---
+++ src/main/java/com/coderscampus/lucasbeckerassignment9/LucasBeckerAssignment9Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.lucasbeckerassignment9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucasBeckerAssignment9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucasBeckerAssignment9Application.class, args);
+	}
+
+}
