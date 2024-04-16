f43e8597ee4418a106a355d54d02b1a67b7b361a
Lucas Becker <lucasbecker.dev@gmail.com>
2024-04-15 22:41:24 -0400

    implemented required RecipeRepository methods

--- src/main/java/com/coderscampus/lucasbeckerassignment9/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coderscampus.lucasbeckerassignment9;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class RecipeRepository {
-}
---
+++ src/main/java/com/coderscampus/lucasbeckerassignment9/repository/RecipeRepository.java
@@ -0,0 +1,15 @@
+package com.coderscampus.lucasbeckerassignment9.repository;
+
+import com.coderscampus.lucasbeckerassignment9.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    List<Recipe> findByGlutenFreeTrue();
+    List<Recipe> findByVeganTrue();
+    List<Recipe> findByVeganTrueAndGlutenFreeTrue();
+    List<Recipe> findByVegetarianTrue();
+}
