0dae15fd805b3ec2af926307b8d0ded9806e07e1
Lucas Becker <lucasbecker.dev@gmail.com>
2024-04-15 22:43:37 -0400

    added @Autowired recipeRepository

--- src/main/java/com/coderscampus/lucasbeckerassignment9/service/RecipeService.java
+++ src/main/java/com/coderscampus/lucasbeckerassignment9/service/RecipeService.java
@@ -1,7 +1,11 @@
 package com.coderscampus.lucasbeckerassignment9.service;
 
+import com.coderscampus.lucasbeckerassignment9.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RecipeService {
+    @Autowired
+    private RecipeRepository recipeRepository;
 }
