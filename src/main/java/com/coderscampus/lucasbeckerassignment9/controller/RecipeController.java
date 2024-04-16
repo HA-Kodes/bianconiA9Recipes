19b24bfd873aa7e58bbb7b87912bb79de45e3a0c
Lucas Becker <lucasbecker.dev@gmail.com>
2024-04-15 23:48:14 -0400

    added all required endpoints

--- src/main/java/com/coderscampus/lucasbeckerassignment9/controller/RecipeController.java
+++ src/main/java/com/coderscampus/lucasbeckerassignment9/controller/RecipeController.java
@@ -1,7 +1,44 @@
 package com.coderscampus.lucasbeckerassignment9.controller;
 
+import com.coderscampus.lucasbeckerassignment9.model.Recipe;
+import com.coderscampus.lucasbeckerassignment9.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RecipeController {
+    private final RecipeService recipeService;
+
+    @Autowired
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/gluten-free")
+    public List<Recipe> getGlutenFreeRecipes() {
+        return recipeService.getGlutenFreeRecipes();
+    }
+
+    @GetMapping("/vegan")
+    public List<Recipe> getVeganRecipes() {
+        return recipeService.getVeganRecipes();
+    }
+
+    @GetMapping("/vegan-and-gluten-free")
+    public List<Recipe> getVeganAndGlutenFreeRecipes() {
+        return recipeService.getVeganAndGlutenFreeRecipes();
+    }
+
+    @GetMapping("/vegetarian")
+    public List<Recipe> getVegetarianRecipes() {
+        return recipeService.getVegetarianRecipes();
+    }
+
+    @GetMapping("/all-recipes")
+    public List<Recipe> getAllRecipes() {
+        return recipeService.getAllRecipes();
+    }
 }
