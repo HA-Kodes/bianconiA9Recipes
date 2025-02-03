c6afa42571eca727b9f6bc4bbe28043e0abb8272
HA-Kodes <2BianconiHa@gmail.com>
2025-02-03 04:11:46 -0800

    minor fix

--- src/main/java/com/coderscampus/controller/RecipeController.java
+++ src/main/java/com/coderscampus/controller/RecipeController.java
@@ -1,6 +1,7 @@
 package com.coderscampus.controller;
 
 import com.coderscampus.model.Recipe;
+import com.coderscampus.model.RecipeResponse;
 import com.coderscampus.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class RecipeController {
@@ -17,28 +20,43 @@ public class RecipeController {
     private RecipeService recipeService;
 
     @GetMapping("/all-recipes")
-    public List<Recipe> getAllRecipes() {
-        return recipeService.getAllRecipes();
+    public RecipeResponse getAllRecipes() {
+        List<Recipe> recipes = recipeService.getAllRecipes();
+        Map<String, List<Recipe>> categorizedRecipes = new HashMap<>();
+        categorizedRecipes.put("All Recipes", recipes);
+        return new RecipeResponse("All recipes retrieved successfully", categorizedRecipes);
     }
 
     @GetMapping("/gluten-free")
-    public List<Recipe> getGlutenFreeRecipes() {
-        return recipeService.getGlutenFreeRecipes();
+    public RecipeResponse getGlutenFreeRecipes() {
+        List<Recipe> recipes = recipeService.getGlutenFreeRecipes();
+        Map<String, List<Recipe>> categorizedRecipes = new HashMap<>();
+        categorizedRecipes.put("Gluten-Free Recipes", recipes);
+        return new RecipeResponse("Gluten-free recipes retrieved successfully", categorizedRecipes);
     }
 
     @GetMapping("/vegan")
-    public List<Recipe> getVeganRecipes() {
-        return recipeService.getVeganRecipes();
+    public RecipeResponse getVeganRecipes() {
+        List<Recipe> recipes = recipeService.getVeganRecipes();
+        Map<String, List<Recipe>> categorizedRecipes = new HashMap<>();
+        categorizedRecipes.put("Vegan Recipes", recipes);
+        return new RecipeResponse("Vegan recipes retrieved successfully", categorizedRecipes);
     }
 
     @GetMapping("/vegan-and-gluten-free")
-    public List<Recipe> getVeganAndGlutenFreeRecipes() {
-        return recipeService.getVeganAndGlutenFreeRecipes();
+    public RecipeResponse getVeganAndGlutenFreeRecipes() {
+        List<Recipe> recipes = recipeService.getVeganAndGlutenFreeRecipes();
+        Map<String, List<Recipe>> categorizedRecipes = new HashMap<>();
+        categorizedRecipes.put("Vegan and Gluten-Free Recipes", recipes);
+        return new RecipeResponse("Vegan and gluten-free recipes retrieved successfully", categorizedRecipes);
     }
 
     @GetMapping("/vegetarian")
-    public List<Recipe> getVegetarianRecipes() {
-        return recipeService.getVegetarianRecipes();
+    public RecipeResponse getVegetarianRecipes() {
+        List<Recipe> recipes = recipeService.getVegetarianRecipes();
+        Map<String, List<Recipe>> categorizedRecipes = new HashMap<>();
+        categorizedRecipes.put("Vegetarian Recipes", recipes);
+        return new RecipeResponse("Vegetarian recipes retrieved successfully", categorizedRecipes);
     }
 
     @PostMapping("/update-recipe")
