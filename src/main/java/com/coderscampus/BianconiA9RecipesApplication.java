bc83d6343e9d1dc4b35dfd5efe6f2a17857fb737
HA-Kodes <2BianconiHa@gmail.com>
2025-02-02 16:18:34 -0800

    Initial commit

---
+++ src/main/java/com/coderscampus/BianconiA9RecipesApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BianconiA9RecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BianconiA9RecipesApplication.class, args);
+	}
+
+}
